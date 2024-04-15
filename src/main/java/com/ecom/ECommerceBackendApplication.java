ec1c02042dbf01e2ae84afaf2337df0edef8fd25
karunkumar <karunkumar@CLAYFIN-LP074>
2024-04-15 12:12:09 +0530

    Added SpringBootServletInitializer

--- src/main/java/com/ecom/ECommerceBackendApplication.java
+++ src/main/java/com/ecom/ECommerceBackendApplication.java
@@ -2,12 +2,19 @@ package com.ecom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ECommerceBackendApplication {
+public class ECommerceBackendApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ECommerceBackendApplication.class, args);
 	}
+	
+	@Override
+	   protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	      return application.sources(ECommerceBackendApplication.class);
+	   }
 
 }
